5e814746ce10fa30f6b9e1e969b6b8fe49086c50
sakai <sakai63016@gmail.com>
2024-07-22 12:25:27 +0800

    添加activiti模块，准备引入工作流

---
+++ srm-activiti/src/test/java/com/srm/activiti/SrmActivitiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.srm.activiti;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SrmActivitiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
