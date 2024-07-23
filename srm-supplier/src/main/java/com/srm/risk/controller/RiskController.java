8b1e2f23014e22f1f03ec64ce4c6f04eb0e4b09d
Sakai <sakai263016@gmail.com>
2024-07-23 20:03:37 +0800

    添加注释

--- srm-supplier/src/main/java/com/srm/risk/controller/RiskController.java
+++ srm-supplier/src/main/java/com/srm/risk/controller/RiskController.java
@@ -19,18 +19,32 @@ public class RiskController {
     @Autowired
     private IRiskService riskService;
 
-
+    /**
+     * 获取税务风险
+     * @param supplierName 供应商名称
+     * @return TaxResponse
+     */
     @GetMapping("/tax-data")
     public TaxResponse taxData(@RequestParam String supplierName) {
         return riskService.executeGet(supplierName);
     }
 
+    /**
+     * 获取经营风险
+     * @param supplierName 供应商名称
+     * @return OperateResponse
+     */
     @GetMapping("/operate-data")
     public OperateResponse operateData(@RequestParam String supplierName) {
         OperateResponse operateResponse = riskService.executeGetOperate(supplierName);
         return operateResponse;
     }
 
+    /**
+     * 获取违法风险
+     * @param supplierName 供应商名称
+     * @return IllegalResponse
+     */
     @GetMapping("/illegal-data")
     public IllegalResponse illegalData(@RequestParam String supplierName) {
         IllegalResponse illegalResponse = riskService.executeGetIllegal(supplierName);
