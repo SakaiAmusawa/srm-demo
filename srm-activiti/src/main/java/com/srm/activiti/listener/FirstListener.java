e16dbbe7b6e6e2a09e554195de6ec5807a89ccc4
Sakai <sakai263016@gmail.com>
2024-07-24 19:26:02 +0800

    activiti监听器的使用

---
+++ srm-activiti/src/main/java/com/srm/activiti/listener/FirstListener.java
@@ -0,0 +1,17 @@
+package com.srm.activiti.listener;
+
+import lombok.extern.slf4j.Slf4j;
+import org.activiti.engine.delegate.DelegateTask;
+import org.activiti.engine.delegate.TaskListener;
+
+@Slf4j
+public class FirstListener implements TaskListener {
+    @Override
+    public void notify(DelegateTask delegateTask) {
+        log.debug("监听器被执行了......");
+        if (EVENTNAME_CREATE.equals(delegateTask.getEventName())) {
+            //表示创建时间被触发了
+            delegateTask.setAssignee("zjw");
+        }
+    }
+}
